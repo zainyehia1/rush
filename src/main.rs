b7785778b05927203bbec922d69e236f36faaab4
Zain Yehia <zainyehia1@gmail.com>
2026-05-21 16:56:02 +0300

    Implement support for single quotes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::env;
 use std::path;
@@ -17,22 +18,22 @@ fn main() {
         input = input.trim().to_string();
         let path = env::var("PATH").unwrap();
 
-        let args: Vec<&str> = input.split_whitespace().collect();
-        
+        let args = parse_args(&input);
+                
         if args[0] == "exit" {
             break;
         } else if args[0] == "echo" {
-            println!("{}", &input[5..]);
+            println!("{}", args[1..].join(" "));
         } else if args[0] == "type" {
-            if builtin_commands.contains(&args[1]) {
+            if builtin_commands.contains(&args[1].as_str()) {
                 println!("{} is a shell builtin", &args[1]);
             } else if let Some(path) = locate_executables(&args[1], &path){
                 println!("{} is {}", &args[1], path.display())
             } else {
                 println!("{}: not found", &args[1]);
             }
-        } else if locate_executables(args[0], &path).is_some() {
-            std::process::Command::new(args[0]).args(&args[1..]).spawn().unwrap().wait().unwrap();
+        } else if locate_executables(&args[0].as_str(), &path).is_some() {
+            std::process::Command::new(&args[0].as_str()).args(&args[1..]).spawn().unwrap().wait().unwrap();
         }
             else {
             println!("{}: command not found", input.trim());
@@ -49,4 +50,31 @@ fn locate_executables(command: &str, path: &str) -> Option<path::PathBuf> {
             false
         }
     })
+}
+
+fn parse_args(input: &str) -> Vec<String>{
+    let mut in_single_quote = false;
+    let mut args = Vec::new();
+    let mut current = String::new();
+    
+    for char in input.chars() {
+        if char == '\'' {
+            in_single_quote = !in_single_quote;
+        } else if !char.is_whitespace() && !in_single_quote {
+            current.push(char);
+        } else if char.is_whitespace() && !in_single_quote {
+            if !current.is_empty() {
+                args.push(current.clone());
+                current.clear();
+            }
+        } else {
+            current.push(char);
+        }
+    }
+
+    if !current.is_empty() {
+        args.push(current);
+    }
+    
+    args
 }
