cc18e7dfe6f3d2f50a47defe98b8b85197a7fe79
Zain Yehia <zainyehia1@gmail.com>
2026-06-04 21:54:50 +0300

    refactor: use "if let" for destructuring a single pattern instead of 'match'

--- src/history.rs
+++ src/history.rs
@@ -3,29 +3,23 @@ use std::fs;
 use std::io::{Read, Write};
 
 pub fn load_history(history: &mut Vec<String>) {
-    match env::var("HISTFILE") {
-        Ok(path) => {
-            let mut file = fs::File::open(path).unwrap();
-            let mut buffer = String::new();
-            file.read_to_string(&mut buffer).unwrap();
+    if let Ok(path) = env::var("HISTFILE") {
+        let mut file = fs::File::open(path).unwrap();
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer).unwrap();
 
-            for line in buffer.lines().filter(|l| !l.is_empty()) {
-                history.push(line.to_string());
-            }
+        for line in buffer.lines().filter(|l| !l.is_empty()) {
+            history.push(line.to_string());
         }
-        Err(_) => ()
     }
 }
 
 pub fn save_history(history: &Vec<String>) {
-    match env::var("HISTFILE") {
-        Ok(path) => {
-            let mut file = std::fs::File::create(path).unwrap();
+    if let Ok(path) = env::var("HISTFILE") {
+        let mut file = std::fs::File::create(path).unwrap();
 
-            for line in history {
-                file.write_all((line.to_string() + "\n").as_bytes()).unwrap();
-            }
+        for line in history {
+            file.write_all((line.to_string() + "\n").as_bytes()).unwrap();
         }
-        Err(_) => ()
     }
 }
